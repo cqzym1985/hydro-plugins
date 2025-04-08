28944f986835d84d772239a5a7c9546c406cd0ad
cqzym1985 <32982433+cqzym1985@users.noreply.github.com>
2025-04-08 12:43:54 +0800

    移除moment依赖

--- countdown/index.ts
+++ countdown/index.ts
@@ -1,23 +1,25 @@
 import { HomeHandler } from 'hydrooj/src/handler/home'
-import moment from 'moment';
 
 async function getCountdown(payload) {
     var content = new Array();
-	var dateToday = moment().format("YYYY-MM-DD");
-	var dates = new Array(payload.dates);
-	dates = dates[0];
-	dates.forEach(function(val, ind) {
-		if (content.length < payload['max_dates']) {
-			if (moment(val.date).isSameOrAfter(dateToday)) {
-				var diffTime = moment(val.date).diff(moment(), 'days');
-				content.push({
-					name: val.name,
-					diff: diffTime
-				})
-			}
-		}
-	});
-	payload.dates = content;
+    var dateToday = new Date();
+    dateToday.setHours(0, 0, 0, 0);
+    var dates = new Array(payload.dates);
+    dates = dates[0];
+    dates.forEach(function(val, ind) {
+        if (content.length < payload['max_dates']) {
+            var targetDate = new Date(val.date);
+            targetDate.setHours(0, 0, 0, 0);
+            if (targetDate >= dateToday) {
+                var diffTime = Math.floor((targetDate - dateToday) / 86400000);
+                content.push({
+                    name: val.name,
+                    diff: diffTime
+                })
+            }
+        }
+    });
+    payload.dates = content;
     return payload;
 }
 HomeHandler.prototype.getCountdown = async (domainId, payload) => {
