436cd1e1b1677d2d16c28c0808143dca789f53ec
cqzym1985 <cqzym1985@163.com>
2026-04-03 19:46:48 +0800

    批量实名认证，修改学生密码等

--- realname-33oj/frontend/foo.page.ts
+++ realname-33oj/frontend/foo.page.ts
@@ -1,8 +1,73 @@
-import { $, addPage, NamedPage, UserSelectAutoComplete } from '@hydrooj/ui-default'
+import { $, addPage, NamedPage, UserSelectAutoComplete, Notification, delay, i18n, url, request } from '@hydrooj/ui-default'
 
-
-addPage(new NamedPage(['realname_set'], () => {
+addPage(new NamedPage(['realname_set','realname_resetpwd'], () => {
     UserSelectAutoComplete.getOrConstruct($('[name="uidOrName"]'), {
         clearDefaultValue: false,
     });
+}));
+
+addPage (new NamedPage('realname_set', () => {
+  $(document).on('click', '[type="submit"]', async (ev) => {
+    ev.preventDefault();
+
+    const $form = $(ev.currentTarget).closest('form');
+    try {
+      const res = await request.post('', {
+        uidOrName: $form.find('[name="uidOrName"]').val(),
+        flag: $form.find('[name="flag"]').val(),
+        name: $form.find('[name="name"]').val(),
+      });
+      if (res.url) {
+        Notification.success(i18n('添加实名成功'));
+        await delay(1000);
+        window.location.href = res.url;  
+      }
+    } catch (e) {
+      Notification.error(e.message);
+    }
+  });
+}));
+
+addPage (new NamedPage('realname_import', () => {
+  async function post(draft) {
+    try {
+      const res = await request.post('', {
+        realnames: $('[name="realnames"]').val(),
+        draft,
+      });
+      if (!draft) {
+        Notification.success(i18n('Updated {0} realname records.', res.realnames.length));
+        await delay(2000);
+        window.location.reload();
+      } else {
+        $('[name="messages"]').text(res.messages.join('\n'));
+      }
+    } catch (e) {
+      Notification.error(e.message);
+    }
+  }
+
+  $('[name="preview"]').on('click', () => post(true));
+  $('[name="submit"]').on('click', () => post(false));
+}));
+
+addPage (new NamedPage('realname_resetpwd', () => {
+  $(document).on('click', '[type="submit"]', async (ev) => {
+    ev.preventDefault();
+
+    const $form = $(ev.currentTarget).closest('form');
+    try {
+      const res = await request.post('', {
+        uidOrName: $form.find('[name="uidOrName"]').val(),
+        resetpwd: $form.find('[name="resetpwd"]').val(),
+      });
+      if (res.url) {
+        Notification.success(i18n('重置密码成功'));
+        await delay(1000);
+        window.location.href = res.url;  
+      }
+    } catch (e) {
+      Notification.error(e.message);
+    }
+  });
 }));
--- realname-33oj/index.ts
+++ realname-33oj/index.ts
@@ -1,67 +1,169 @@
 import {
-    _, db, UserModel, SettingModel, DomainModel, Handler, param, PRIV, Types, query, NotFoundError
+    db, UserModel, SettingModel, DomainModel, Handler, UserNotFoundError, param, PERM, PRIV, Types, query, requireSudo
 } from 'hydrooj';
-
-// 修复 `getListForRender` 函数，给前端传递更多内容
-export const coll: Collection<Udoc> = db.collection('user');
-export const collV: Collection<VUdoc> = db.collection('vuser');
-export const collGroup: Collection<GDoc> = db.collection('user.group');
-UserModel.getListForRender = async function (domainId: string, uids: number[]) {
-    const [udocs, vudocs, dudocs] = await Promise.all([
-        UserModel.getMulti({ _id: { $in: uids } }, ['_id', 'uname', 'mail', 'avatar', 'school', 'studentId', 'realname_flag', 'realname_name']).toArray(),
-        collV.find({ _id: { $in: uids } }).toArray(),
-        DomainModel.getDomainUserMulti(domainId, uids).project({ uid: true, displayName: true }).toArray(),
-    ]);
-    const udict = {};
-    for (const udoc of udocs) udict[udoc._id] = udoc;
-    for (const udoc of vudocs) udict[udoc._id] = udoc;
-    for (const dudoc of dudocs) udict[dudoc.uid].displayName = dudoc.displayName;
-    for (const uid of uids) udict[uid] ||= { ...UserModel.defaultUser };
-    for (const key in udict) {
-        udict[key].school ||= '';
-        udict[key].studentId ||= '';
-        udict[key].displayName ||= udict[key].uname;
-        udict[key].avatar ||= `gravatar:${udict[key].mail}`;
-    }
-    return udict as BaseUserDict;
-};
 
 //实名设置
 class RealnameSetHandler extends Handler {
-    async get() {
+    @query('uidOrName', Types.UidOrName, true)
+    @query('name', Types.string, true)
+    async get(domainId: string, uidOrName: string, name: string) {
         this.response.template = 'realname_set.html'; // 返回此页面
+        this.response.body = { uidOrName, name };
     }
     @param('uidOrName', Types.UidOrName)
-    @param('flag', Types.number)
-    @param('name', Types.string)
+    @param('flag', Types.number, true)
+    @param('name', Types.string, true)
     async post(domainId: string, uidOrName: string, flag: number, name: string) {
         // 检查输入
-        flag = parseInt(flag);
-        let udoc = await UserModel.getById(domainId, +uidOrName)
-                || await UserModel.getByUname(domainId, uidOrName)
-                || await UserModel.getByEmail(domainId, uidOrName);
+        flag = parseInt(flag, 10);
+        const udoc = await UserModel.getById(domainId, +uidOrName)
+            || await UserModel.getByUname(domainId, uidOrName)
+            || await UserModel.getByEmail(domainId, uidOrName);
         if (!udoc)
-            throw new NotFoundError(uidOrName);
-        // 构建徽章代码并更新
-        await UserModel.setById(udoc._id, { realname_flag: flag, realname_name: name });
-        // 将用户重定向到创建完成的url
-        this.response.redirect = "/realname/show";
+            throw new UserNotFoundError(uidOrName);
+        // 构建实名代码并更新
+        if ([0, 1, 2].includes(flag)) await UserModel.setById(udoc._id, { realname_flag: flag });
+        if (name) await UserModel.setById(udoc._id, { realname_name: name.trim() });
+        this.response.redirect = this.url('realname_set');
     }
 }
 
 //展示实名用户
 class RealnameShowHandler extends Handler {
     @query('page', Types.PositiveInt, true)
-    async get(domainId: string, page = 1) {
-        const [dudocs, upcount, ucount] = await this.ctx.db.paginate(
-            UserModel.getMulti({ realname_flag: { $exists: true } }).sort({ realname_flag: -1, _id: -1 }),
+    @query('groupName', Types.string, true)
+    async get(domainId: string, page = 1, groupName: string) {
+
+        let filter = { realname_flag: { $exists: true, $ne: "" } };
+
+        const groups = await UserModel.listGroup(domainId);
+        if (groupName) {
+            const groupInfo = groups.find(g => g.name === groupName);
+            if (groupInfo) {
+                filter._id = { $in: groupInfo.uids };
+            }
+        }
+
+        const [dudocs, upcount] = await this.paginate(
+            UserModel.getMulti(filter).sort({ realname_flag: -1, _id: -1 }),
             page,
-            50,
+            'ranking'
         );
-        const udict = await UserModel.getList("system", dudocs.map((x) => x._id));
+        const udict = await UserModel.getList(domainId, dudocs.map((x) => x._id));
         const udocs = dudocs.map((x) => udict[x._id]);
         this.response.template = 'realname_show.html'; // 返回此页面
-        this.response.body = { udocs, upcount, ucount, page, udict };
+        this.response.body = { udocs, upcount, page, groupName, groups };
+    }
+}
+
+//导入实名用户
+class RealnameImportHandler extends Handler {
+    async get() {
+        this.response.body.realnames = [];
+        this.response.template = 'realname_import.html';
+    }
+
+    @param('realnames', Types.Content)
+    @param('draft', Types.Boolean)
+    async post(domainId: string, _realnames: string, draft: boolean) {
+        const realnames = _realnames.split('\n');
+        const udocs: { username: string, flag: number, name: string }[] = [];
+        const messages = [];
+
+        for (const i in realnames) {
+            const u = realnames[i];
+            if (!u.trim()) continue;
+            let [username, flag, name] = u.split('\t').map((t) => t.trim());
+            if (username && !flag && !name) {
+                const data = u.split(',').map((t) => t.trim());
+                [username, flag, name] = data;
+            }
+
+            if (!username) continue;
+            flag = parseInt(flag, 10);
+
+            // 验证用户是否存在
+            const user = await UserModel.getByUname(domainId, username);
+            if (!user) {
+                messages.push(`Line ${+i + 1}: User ${username} not found.`);
+                continue;
+            }
+
+            udocs.push({
+                username, flag, name
+            });
+        }
+        messages.push(`${udocs.length} realname records found.`);
+
+        if (!draft) {
+            for (const udoc of udocs) {
+                try {
+                    const user = await UserModel.getByUname(domainId, udoc.username);
+                    if (!user) continue;
+
+                    if (![0, 1, 2].includes(udoc.flag) && udoc.name === '') {
+                        await UserModel.setById(user._id, { realname_flag: '', realname_name: '' });
+                    } else {
+                        if ([0, 1, 2].includes(udoc.flag)) {
+                            await UserModel.setById(user._id, { realname_flag: udoc.flag });
+                        }
+                        if (udoc.name) {
+                            await UserModel.setById(user._id, { realname_name: udoc.name });
+                        }
+                    }
+                } catch (e) {
+                    messages.push(e.message);
+                }
+            }
+        }
+        this.response.body.realnames = udocs;
+        this.response.body.messages = messages;
+    }
+}
+
+//修改密码
+class ResetPwdHandler extends Handler {
+    @requireSudo
+    @query('uidOrName', Types.UidOrName, true)
+    async get(domainId: string, uidOrName: string) {
+        this.response.template = 'realname_resetpwd.html'; // 返回此页面
+        this.response.body = { uidOrName };
+    }
+
+    @requireSudo
+    @param('uidOrName', Types.UidOrName)
+    @param('resetpwd', Types.Password)
+    async post(domainId: string, uidOrName: string, resetpwd: string) {
+        // 检查输入
+        const udoc = await UserModel.getById(domainId, +uidOrName)
+            || await UserModel.getByUname(domainId, uidOrName)
+            || await UserModel.getByEmail(domainId, uidOrName);
+        if (!udoc)
+            throw new UserNotFoundError(uidOrName);
+
+        if ((udoc.hasPriv(PRIV.PRIV_SET_PERM) && udoc._id !== this.user._id) || udoc.hasPriv(PRIV.PRIV_EDIT_SYSTEM)) {
+            this.checkPriv(PRIV.PRIV_ALL);
+        }
+        // 修改密码
+        await UserModel.setPassword(udoc._id, resetpwd);
+        this.response.redirect = this.url('realname_resetpwd');
+    }
+}
+
+//快速认证
+class RealnameQuickHandler extends Handler {
+    @query('page', Types.PositiveInt, true)
+    async get(domainId: string, page = 1) {
+        const [dudocs, upcount] = await this.paginate(
+            UserModel.getMulti({ "_id": { $gte: 2 }, $or: [ { "realname_name": { $exists: false } }, { "realname_name": "" } ] }).sort({ realname_flag: 1, _id: -1 }),
+            page,
+            'ranking'
+        );
+        const udict = await UserModel.getList(domainId, dudocs.map((x) => x._id));
+        const udocs = dudocs.map((x) => udict[x._id]);
+
+        this.response.template = 'realname_quick.html'; // 返回此页面
+        this.response.body = { udocs, upcount, page };
     }
 }
 
@@ -69,10 +171,34 @@ class RealnameShowHandler extends Handler {
 export async function apply(ctx: Context) {
     ctx.inject(['setting'], (c) => {
         c.setting.AccountSetting(
-            SettingModel.Setting('setting_33oj', 'realname_flag', 0, 'number', 'realname_flag', null, 3),
-            SettingModel.Setting('setting_33oj', 'realname_name', '', 'text', 'realname_name', null, 3)
+            SettingModel.Setting('setting_info', 'realname_flag', 0, 'number', '身份标记', '0是未添加身份，1是学生，2是老师', 2),
+            SettingModel.Setting('setting_info', 'realname_name', '', 'text', '姓名', null, 2)
         );
-        ctx.Route('realname_set', '/realname/set', RealnameSetHandler, PRIV.PRIV_CREATE_DOMAIN);
-        ctx.Route('realname_show', '/realname/show', RealnameShowHandler, PRIV.PRIV_CREATE_DOMAIN);
+    });
+
+    //修改getListForRender 
+    const originalGetListForRender = UserModel.getListForRender;
+    UserModel.getListForRender = async function(domainId, uids, arg, extraFields) {
+        //添加realname相关字段
+        const _extraFields = Array.isArray(arg) ? arg : Array.isArray(extraFields) ? extraFields : [];
+        const newExtraFields = [..._extraFields, 'realname_name', 'realname_flag']; 
+        if (Array.isArray(arg)) {
+            return originalGetListForRender.call(this, domainId, uids, newExtraFields);
+        }
+        return originalGetListForRender.call(this, domainId, uids, arg, newExtraFields);
+    };
+
+    ctx.Route('realname_show', '/realname/show', RealnameShowHandler);
+    ctx.Route('realname_set', '/realname/set', RealnameSetHandler, PRIV.PRIV_SET_PERM);
+    ctx.Route('realname_import', '/realname/import', RealnameImportHandler, PRIV.PRIV_SET_PERM);
+    ctx.Route('realname_quick', '/realname/quick', RealnameQuickHandler, PRIV.PRIV_SET_PERM);
+    ctx.Route('realname_resetpwd', '/realname/resetpwd', ResetPwdHandler, PRIV.PRIV_SET_PERM);
+    ctx.injectUI('UserDropdown', 'realname_show', { icon: 'feeling-lucky', displayName: '实名认证' });
+    ctx.i18n.load('zh', {
+        realname_show: '查看实名认证',
+        realname_set: '添加实名认证',
+        realname_import: '批量实名认证',
+        realname_quick: '快速实名认证',
+        realname_resetpwd: '重置密码',
     });
 }
